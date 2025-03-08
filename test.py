50e05e05a5450acf8420b2e4144636bb703b01fe
weiouo-0817 <jack930817.ee11@nycu.edu.tw>
2025-03-08 11:05:01 -0500

    add test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def Towers(n, fr, to, spare , count1):
         count1 += 1
         
     else:
-        count1=Towers(n-1, fr, spare, to, count1)
+        Towers(n-1, fr, spare, to, count1)
         Towers(1, fr, to, spare, count1)
         Towers(n-1, spare, to, fr, count1)
     return count1
